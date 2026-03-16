c7e3412bc8c7165358b886d9d7b042205a4971f6
Pedro <pedroppaf@gmail.com>
2026-03-16 12:18:39 -0300

    create form logic

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,16 +1,38 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { PasswordField } from '../../shared/components/password-field/password-field';
+import { form } from '@angular/forms/signals';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, PasswordField],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, PasswordField, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
   encapsulation: ViewEncapsulation.None
 })
-export class Register {}
+export class Register {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group({
+      fullName: [''],
+      email: [''],
+      password: ['']
+    });
+  }
+
+  get passwordControl(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
+
+  submit(){
+    console.log(this.form.value)
+  }
+}
+
+
