43cd68b4f8e1b1dd6ccac39f88da796709aebc87
Pedro <pedroppaf@gmail.com>
2026-03-24 16:51:23 -0300

    create home and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopMenu } from "./shared/components/global/top-menu/top-menu";
+import { Footer } from "./shared/components/global/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopMenu],
+  imports: [RouterOutlet, TopMenu, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
